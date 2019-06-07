632680fd30053f78b9d41fe11bd24ca6b403e7c8
Eunsung Lee <okskmk2@naver.com>
2019-06-08 01:49:09 +0900

    react 추가

--- backend/fbank/settings.py
+++ backend/fbank/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'fbank.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,5 +125,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_URL = '/static/'
